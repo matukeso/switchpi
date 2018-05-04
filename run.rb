c269d941efb552eaa1cb48d09dd9d6a94eccbd77
matuken <matuken1@jzkey.com>
2018-05-04 21:56:14 +0900

    runner script

---
+++ run.rb
@@ -0,0 +1,27 @@
+$mountpoint = "/media/pi".freeze
+
+dirs = Dir.glob("#{$mountpoint}/*")
+
+dirs.delete_if {|d|
+  s = File.stat(d)
+  s.file?
+}
+  
+
+if( dirs.empty? )
+  puts "no usb disk";
+end
+
+
+dir = dirs[0];
+puts("usb disk select #{dir}");
+
+log =  Time.now.strftime("%Y%m%dT%H%M%S")
+
+
+abspath = dir + "/" + log + ".log"
+puts("file is #{abspath}");
+
+
+
+
