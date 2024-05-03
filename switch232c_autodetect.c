d93582af7ea93948939dfe5368f955fe734652e3
matuken <mautken@jzkey.com>
2024-05-03 13:05:37 +0900

    [add] blakmagic lan mode

--- switch232c_autodetect.c
+++ switch232c_autodetect.c
@@ -18,9 +18,15 @@ enum
 {
 	MODE_V160 = 160,
 	MODE_V1SD = 1
+	MODE_BLACKMAGIC = 111
 };
 
 int controller_232c_mode = 0;
+extern void set_bm_mode()
+{
+	controller_232c_mode = MODE_BLACKMAGIC;
+}
+
 
 extern void send_232c_pgm_v160(int ch);
 extern void send_232c_pgm_v1(int ch);
@@ -33,12 +39,21 @@ extern int check_v1sd1(int fd, int fdlog);
 extern int loop_switch232c_v160(int fd, int fdlog);
 extern int loop_switch232c_v1(int fd, int fdlog);
 
+extern int init_bm_lan(const char *peer);
+extern int bm_lan_loop(int fd, int fdlog);
+extern void send_232c_pgm_bmlan(int ch);
+extern void send_232c_pst_bmlan(int ch);
+extern void send_232c_ato_bmlan();
+
+
 void send_232c_pgm(int ch)
 {
 	if (controller_232c_mode == MODE_V160)
 		send_232c_pgm_v160(ch);
 	if (controller_232c_mode == MODE_V1SD)
 		send_232c_pgm_v1(ch);
+	if (controller_232c_mode == MODE_BLACKMAGIC)
+		send_232c_pgm_bmlan(ch);
 }
 void send_232c_pst(int ch)
 {
@@ -46,6 +61,8 @@ void send_232c_pst(int ch)
 		send_232c_pst_v160(ch);
 	if (controller_232c_mode == MODE_V1SD)
 		send_232c_pst_v1(ch);
+	if (controller_232c_mode == MODE_BLACKMAGIC)
+		send_232c_pst_bmlan(ch);
 }
 void send_232c_ato()
 {
@@ -53,6 +70,8 @@ void send_232c_ato()
 		send_232c_ato_v160();
 	if (controller_232c_mode == MODE_V1SD)
 		send_232c_ato_v1();
+	if (controller_232c_mode == MODE_BLACKMAGIC)
+		send_232c_ato_bmlan();
 }
 
 int loop_switch232c(int fd, int fdlog)
@@ -70,6 +89,7 @@ int loop_switch232c(int fd, int fdlog)
 			controller_232c_mode = MODE_V1SD;
 			loop_switch232c_v1(fd, fdlog);
 		}
+
 		nanosleep(&ts, &rem);
 	}
 }
